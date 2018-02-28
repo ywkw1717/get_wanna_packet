2564547217ddf480be708b13baf2ecc3eb3e27b8
yyy <y.w.k.w.1717@gmail.com>
2018-02-28 19:23:43 +0900

    [Add] thread

--- get_wanna_packet.py
+++ get_wanna_packet.py
@@ -1,8 +1,10 @@
 import subprocess
 import sys
+import threading
 
 
 def start(filename):
+    print('----- {} -----'.format(filename))
     cmd = subprocess.getoutput("tshark -r " + filename + " -Y 'tcp.port == 445 and not smb2 and smb contains 00:31:00:39:00:32:00:2e:00:31:00:36:00:38:00:2e:00:35:00:36:00:2e:00:32:00:30 or smb contains 00:31:00:37:00:32:00:2e:00:31:00:36:00:2e:00:39:00:39:00:2e:00:35' -T fields -e tcp.stream |sort -un")
 
     if not cmd:
@@ -18,6 +20,7 @@ def start(filename):
     result_file = filename[0:-5] + "_finish.pcap"
     result = subprocess.getoutput("tshark -r ./" + filename + " -Y '" + option + "' -w " + result_file)
     print(result)
+    print('finish.\n')
 
 
 def main():
@@ -28,12 +31,13 @@ def main():
         print("Please specify list of pcap file.")
         return
 
+    threads = []
     l = f.readlines()
     for i in l:
         filename = i.rstrip()
-        print('----- {} -----'.format(filename))
-        start(filename)
-        print('finish.\n')
+        t = threading.Thread(target=start, args=(filename,))
+        threads.append(t)
+        t.start()
 
 
 if __name__ == "__main__":
